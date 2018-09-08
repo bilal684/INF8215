9406553d1587f74aed7594b33fd5341f56f07361
Bilal <bilal.itani1234@gmail.com>
2018-09-08 12:22:01 -0400

    Added part1.py

---
+++ TP1/src/part1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  8 10:43:22 2018
+
+@author: bitani
+"""
+
+import numpy as np
+
+import copy
+
+#from queue import Queue
+
+def bfs(graph, places):
+    """
+    Returns the best solution which spans over all attractions indicated in 'places'
+    """
+    #theSolution = Solution(places, graph)
+    
+
+class Solution:
+    def __init__(self, places, graph):
+        """
+        places: a list containing the indices of attractions to visit
+        p1 = places[0]
+        pm = places[-1]
+        """
+        self.g = 0 # current cost
+        self.graph = graph 
+        self.visited = [places[0]] # list of already visited attractions
+        self.not_visited = copy.deepcopy(places[1:]) # list of attractions not yet visited
+        
+    def add(self, idx):
+        """
+        Adds the point in position idx of not_visited list to the solution
+        """
+        self.g += graph[self.visited[-1], self.not_visited[idx]]
+        self.visited.append(self.not_visited.pop(idx))
+        
+        
+
+def read_graph():
+    return np.loadtxt("montreal", dtype='i', delimiter=',')
+
+graph = read_graph()
+
