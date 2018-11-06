8d0feb35c0fb9db26c12fd0775a5daafc34bf5cd
bilal itani <bilal.itani1234@gmail.com>
2018-11-06 13:11:13 -0500

    Fixed q3.pl (renamed prediactes).

--- TP2/q3.pl
+++ TP2/q3.pl
@@ -1,43 +1,38 @@
 %Question3
 
 %A is corequisite of B when B is corequisite of A
-corequisite2(A,B) :- corequisite(B,A).
+corequisite(A,B) :- f_corequisite(B,A).
 
-corequisite2(A,B) :- corequisite(A, X), corequisite2(X, B).
+corequisite(A,B) :- f_corequisite(A, X), corequisite(X, B).
 
-corequisite2(A,B) :- requisite(A, X), corequisite2(X, B).
-
-%corequisite2(A,B) :- 
+corequisite(A,B) :- requisite(A, X), corequisite(X, B).
 
 %A is a requisite of B when A is a prerequisite of B or A is a corequisite of B.
-requisite(A,B) :- prerequisite(A,B) ; corequisite(A,B).
+requisite(A,B) :- f_prerequisite(A,B) ; f_corequisite(A,B).
 %A is also a requisite of B when A is a prerequisite of X and X is a requisite of B.
-requisite(A,B) :- prerequisite(A,X), requisite(X,B).
+requisite(A,B) :- f_prerequisite(A,X), requisite(X,B).
 %A is also a requisite of B if A is a corequisite of X and X is a requisite of B.
-requisite(A,B) :- corequisite(A,X), requisite(X,B).
+requisite(A,B) :- f_corequisite(A,X), requisite(X,B).
 
 %To get all the requisites for a given course, we use the setof method which removes duplicates (http://www.swi-prolog.org/pldoc/man?predicate=setof/3)
-getAllRequisitesFor(A, List2) :- setof(X, (requisite(X,A); corequisite2(X,A)), List), delete(List, A, List2).
+getAllRequisitesFor(A, List2) :- setof(X, (requisite(X,A); corequisite(X,A)), List), delete(List, A, List2).
 
-
-
-
-%getAllRequisitesFor(A, List) :- setof(X, corequisite(X,A), List).
+%% Knowledge Base (FACTS) %%
 
 %A is prerequisite of B when prequesite(A,B)
-prerequisite('INF1005c', 'INF1010').
-prerequisite('INF1005c', 'LOG1000').
-prerequisite('INF1005c', 'INF1600').
-prerequisite('INF1500', 'INF1600').
-prerequisite('INF1010', 'INF2010').
-prerequisite('INF1010', 'LOG2410').
-prerequisite('LOG1000', 'LOG2410').
-prerequisite('INF2010', 'INF2705').
+f_prerequisite('INF1005c', 'INF1010').
+f_prerequisite('INF1005c', 'LOG1000').
+f_prerequisite('INF1005c', 'INF1600').
+f_prerequisite('INF1500', 'INF1600').
+f_prerequisite('INF1010', 'INF2010').
+f_prerequisite('INF1010', 'LOG2410').
+f_prerequisite('LOG1000', 'LOG2410').
+f_prerequisite('INF2010', 'INF2705').
 
 %A is corequisite of B when corequisite(A,B)
-corequisite('LOG2810', 'INF2010').
-corequisite('MTH1007', 'INF2705').
-corequisite('LOG2990', 'INF2705').
-corequisite('INF1600', 'INF1900').
-corequisite('LOG1000', 'INF1900').
-corequisite('INF2205', 'INF1900').
+f_corequisite('LOG2810', 'INF2010').
+f_corequisite('MTH1007', 'INF2705').
+f_corequisite('LOG2990', 'INF2705').
+f_corequisite('INF1600', 'INF1900').
+f_corequisite('LOG1000', 'INF1900').
+f_corequisite('INF2205', 'INF1900').
