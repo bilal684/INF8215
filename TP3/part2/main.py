db0de04d0353a6e6e512ff13d47fc089cb30a9fc
bilal itani <bilal.itani1234@gmail.com>
2018-11-23 18:13:29 -0500

    Milestone part 2.

--- TP3/part2/main.py
+++ TP3/part2/main.py
@@ -1,5 +1,12 @@
+import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from preprocessing import TransformationWrapper
+from preprocessing import LabelEncoderP
+import math
 
 PATH = "C:\\Users\\bitani\\Desktop\\INF8215\\TP3\\data\\"
 X_train = pd.read_csv(PATH + "train.csv")
@@ -22,19 +29,38 @@ X_train1.head()
 X_train = X_train.drop(columns = ["Color","Name","DateTime"])
 X_test = X_test.drop(columns = ["Color","Name","DateTime"])
 
-def convertAgeUponTimeToDays(text):
-    number, timeFrame = text.split(" ")
-    if timeFrame.lower() == "year" or timeFrame.lower() == "years":
-        return float(number) * 52.0 #52 weeks per year
-    elif timeFrame.lower() == "month" or timeFrame.lower() == "months":
-        return float(number) * 4.0 #4 weeks per month
-    elif timeFrame.lower() == "week" or timeFrame.lower() == "weeks":
-        return float(number) # 1 week per week
-    elif timeFrame.lower() == "day" or timeFrame.lower() == "days":
-        return float(number) * 1.0/7.0 #because there are 7 days per week
+def convertAgeUponOutcomeToWeeks(text):
+    if (not isinstance(text, str)) and math.isnan(text):
+       return 0.0
+    else:
+        number, timeFrame = text.split(" ")
+        if timeFrame.lower() == "year" or timeFrame.lower() == "years":
+            return float(number) * 52.1428228589286 #52 weeks per year
+        elif timeFrame.lower() == "month" or timeFrame.lower() == "months":
+            return float(number) * 4.34524 #4 weeks per month
+        elif timeFrame.lower() == "week" or timeFrame.lower() == "weeks":
+            return float(number) # 1 week per week
+        elif timeFrame.lower() == "day" or timeFrame.lower() == "days":
+            return float(number) * 1.0/7.0 #because there are 7 days per week
 
-X_train["AgeuponOutcome"].value_counts()/len(X_train)
+pipeline_ageuponoutcome_changeToWeeks = Pipeline([
+    ('mohammed', TransformationWrapper(transformation=convertAgeUponOutcomeToWeeks)),
+    ('mohammed2', StandardScaler())
+])
+
+full_pipeline = ColumnTransformer(
+    [
+        ("bilal", pipeline_ageuponoutcome_changeToWeeks, "AgeuponOutcome"),
+    ], remainder='passthrough'
+)
+
+columns = ["AgeuponOutcome", "AnimalType", "SexuponOutcome", "Breed"]
+#columns = ["AgeuponOutcome"]
+X_train = pd.DataFrame(full_pipeline.fit_transform(X_train), columns= columns)
 
 
+t = X_train["AgeuponOutcome"].value_counts()/len(X_train)
 
-#print(convertAgeUponTimeToDays("10 day"))
+print(t)
+
+#print(convertAgeUponOutcomeToWeeks("10 months"))
